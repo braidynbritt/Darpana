193081550d474d729a1d614f410b968821bb4667
Braidyn Britt <braidynbritt@gmail.com>
2023-01-17 22:08:58 -0600

    Changed explicit declarations to var and switched String.Concat() to string interpolation

--- Darpana/MainWindow.xaml.cs
+++ Darpana/MainWindow.xaml.cs
@@ -58,9 +58,9 @@ namespace Darpana
         //Gets current weather from OpenWeatherMap API Json file
         public void GetCurrWeather()
         {
-            string currWeather = GetCurrWeatherInfo().Result; //Gets json file from OpenWeatherMap
-            WeatherInfo currInfo = JsonConvert.DeserializeObject<WeatherInfo>(currWeather); //Converts Json file into appropriate types
-            Uri iconUrl = new Uri(String.Concat("http://openweathermap.org/img/wn/", currInfo.Weather.Rows[0][3].ToString(), "@2x.png")); //Gets Icon image from OpenWeatherMap
+            var currWeather = GetCurrWeatherInfo().Result; //Gets json file from OpenWeatherMap
+            var currInfo = JsonConvert.DeserializeObject<WeatherInfo>(currWeather); //Converts Json file into appropriate types
+            var iconUrl = new Uri($"http://openweathermap.org/img/wn/{currInfo.Weather.Rows[0][3]}@2x.png"); //Gets Icon image from OpenWeatherMap
             UpdateCurrWeather(currInfo, iconUrl); //Updates the display with new data
 
         }
@@ -69,17 +69,17 @@ namespace Darpana
         public void UpdateCurrWeather(WeatherInfo currInfo, Uri iconUrl)
         {
             //These filter out the needed text from the new data for the TextBlocks in the XAML file
-            string city = currInfo.Name.ToString();
-            string conditions = currInfo.Weather.Rows[0][1].ToString();
-            string[] temp = currInfo.Main["temp"].ToString().Split('.');
-            string[] maxTemp = currInfo.Main["temp_max"].ToString().Split('.');
-            string[] minTemp = currInfo.Main["temp_min"].ToString().Split('.');
+            var city = currInfo.Name;
+            var conditions = currInfo.Weather.Rows[0][1].ToString();
+            var temp = currInfo.Main["temp"].Split('.');
+            var maxTemp = currInfo.Main["temp_max"].Split('.');
+            var minTemp = currInfo.Main["temp_min"].Split('.');
 
             //This updates the textblocks within the XAML file to show correct data
-            City.Text = String.Concat(city, ", IA");
-            CurrTemp.Text = String.Concat(temp[0], "°F");
-            MaxTemp.Text = String.Concat(maxTemp[0], "°F");
-            MinTemp.Text = String.Concat(minTemp[0], "°F");
+            City.Text = $"{city}, IA";
+            CurrTemp.Text = $"{temp[0]}°F";
+            MaxTemp.Text = $"{maxTemp[0]}°F";
+            MinTemp.Text = $"{minTemp[0]}°F";
             CurrConditions.Text = conditions;
             WeatherIcon.Source = new BitmapImage(iconUrl);
         }
@@ -87,8 +87,8 @@ namespace Darpana
         //Gets 5 day forecast
         public void GetForecast()
         {
-            string forecastData = GetForecastInfo().Result; //Gets results from Json forecast file
-            ForecastInfo forecastInfo = JsonConvert.DeserializeObject<ForecastInfo>(forecastData); //Deserializes json file into appropriate types
+            var forecastData = GetForecastInfo().Result; //Gets results from Json forecast file
+            var forecastInfo = JsonConvert.DeserializeObject<ForecastInfo>(forecastData); //Deserializes json file into appropriate types
 
             UpdateForecast(forecastInfo, 1); //Updates next day forecast
             UpdateForecast(forecastInfo, 2); //Updates forecast for 2 days later
@@ -98,13 +98,13 @@ namespace Darpana
         //Updates XAML textboxes for forecast with new data. Forecast updates every 3 hours until 5 days is achieved
         public void UpdateForecast(ForecastInfo forecastInfo, int days)
         {
-            int hours = days * 8; //Multiply days by 8. This will get either 8 or 16. The 3 hours multipled by 8 or 16 gives 24 or 48 hours ahead
-            Dictionary<string, string> forecastMain = ToDictionary<string>(forecastInfo.ForecastList[days]["main"]); //Makes main forecast items (temps) a dictionary.
-            DataTable forecastWeather = ToDataTable(forecastInfo.ForecastList[days]["weather"]); //Makes weather into a dataTable
-            string[] forecastTempMax = forecastMain["temp_max"].ToString().Split('.'); //Filter portion of string out to get integer temp
-            string[] forecastTempMin = forecastMain["temp_min"].ToString().Split('.');
-            Uri iconUrl = new Uri(String.Concat("http://openweathermap.org/img/wn/", forecastWeather.Rows[0][3].ToString(), "@2x.png")); //Get weather condition icon from OpenWeatherMap
-            DateTime dateValue = CreateDate(forecastInfo, hours); //Runs CreateDate method to update day of week
+            var hours = days * 8; //Multiply days by 8. This will get either 8 or 16. The 3 hours multipled by 8 or 16 gives 24 or 48 hours ahead
+            var forecastMain = ToDictionary<string>(forecastInfo.ForecastList[days]["main"]); //Makes main forecast items (temps) a dictionary.
+            var forecastWeather = ToDataTable(forecastInfo.ForecastList[days]["weather"]); //Makes weather into a dataTable
+            var forecastTempMax = forecastMain["temp_max"].Split('.'); //Filter portion of string out to get integer temp
+            var forecastTempMin = forecastMain["temp_min"].Split('.');
+            var iconUrl = new Uri($"http://openweathermap.org/img/wn/{forecastWeather.Rows[0][3]}@2x.png"); //Get weather condition icon from OpenWeatherMap
+            var dateValue = CreateDate(forecastInfo, hours); //Runs CreateDate method to update day of week
 
 
             //If only one day ahead
@@ -112,8 +112,8 @@ namespace Darpana
             {
                 //Update Textboxes in XAML file to new data.
                 ForecastDT1.Text = dateValue.ToString("ddd"); //Shows day of week abbreviation
-                ForecastTempMax1.Text = String.Concat(forecastTempMax[0], "°F");
-                ForecastTempMin1.Text = String.Concat(forecastTempMin[0], "°F");
+                ForecastTempMax1.Text = $"{forecastTempMax[0]}°F";
+                ForecastTempMin1.Text = $"{forecastTempMin[0]}°F";
                 ForecastIcon1.Source = new BitmapImage(iconUrl);
             }
 
@@ -122,8 +122,8 @@ namespace Darpana
             {
                 //Update Textboxes in XAML file to new data
                 ForecastDT2.Text = dateValue.ToString("ddd"); //Shows day of week abbreviation
-                ForecastTempMax2.Text = String.Concat(forecastTempMax[0], "°F");
-                ForecastTempMin2.Text = String.Concat(forecastTempMin[0], "°F");
+                ForecastTempMax2.Text = $"{forecastTempMax[0]}°F";
+                ForecastTempMin2.Text = $"{forecastTempMin[0]}°F";
                 ForecastIcon2.Source = new BitmapImage(iconUrl);
             }
 
@@ -133,16 +133,16 @@ namespace Darpana
         public DateTime CreateDate(ForecastInfo forecastInfo, int time)
         {
             //Splits date text into necessary pieces to then parse for DateTime
-            string forecastDate1 = forecastInfo.ForecastList[time]["dt_txt"].ToString();
-            string[] splitDate1 = forecastDate1.Split('-'); //splits year, month, and day apart
-            string[] splitDay1 = splitDate1[2].Split(' '); //splits day from time
+            var forecastDate1 = forecastInfo.ForecastList[time]["dt_txt"].ToString();
+            var splitDate1 = forecastDate1.Split('-'); //splits year, month, and day apart
+            var splitDay1 = splitDate1[2].Split(' '); //splits day from time
 
             //parses everything to an into to allow for DateTime constructor to work
-            Int32.TryParse(splitDate1[0], out int year);
-            Int32.TryParse(splitDate1[1], out int month);
-            Int32.TryParse(splitDay1[0], out int day);
+            Int32.TryParse(splitDate1[0], out var year);
+            Int32.TryParse(splitDate1[1], out var month);
+            Int32.TryParse(splitDay1[0], out var day);
 
-            DateTime dateValue = new DateTime(year, month, day); //Gets weekday of inserted date
+            var dateValue = new DateTime(year, month, day); //Gets weekday of inserted date
             return dateValue;
         }
 
@@ -150,19 +150,19 @@ namespace Darpana
         public void StartTimers()
         {
             //DispatcherTimer for Time event. Updates every 500 milliseconds
-            DispatcherTimer dispatcherTimerTime = new DispatcherTimer();
+            var dispatcherTimerTime = new DispatcherTimer();
             dispatcherTimerTime.Tick += new EventHandler(DispatcherTimer_Time);
             dispatcherTimerTime.Interval = new TimeSpan(0, 0, 0, 0, 500);
             dispatcherTimerTime.Start();
             
             //DispatcherTimer for current weather event. Updates every 30 minutes.
-            DispatcherTimer dispatcherTimerWeather = new DispatcherTimer();
+            var dispatcherTimerWeather = new DispatcherTimer();
             dispatcherTimerWeather.Tick += new EventHandler(DispatcherTimer_Weather);
             dispatcherTimerWeather.Interval = new TimeSpan(0, 0, 1800);
             dispatcherTimerWeather.Start();
 
             //DispatcherTimer for forecast event. Updates every 3 hours.
-            DispatcherTimer dispatcherTimerForecast = new DispatcherTimer();
+            var dispatcherTimerForecast = new DispatcherTimer();
             dispatcherTimerWeather.Tick += new EventHandler(DispatcherTimer_Forecast);
             dispatcherTimerWeather.Interval = new TimeSpan(0, 0, 10800);
             dispatcherTimerWeather.Start();
@@ -174,10 +174,9 @@ namespace Darpana
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://pro.openweathermap.org/data/2.5/"); //API URL for client
             var query = $"weather?lat=42.6411&lon=-95.2097&APPID={OWAPIKEY}&units=imperial"; //Api specifics. Gets location of Storm Lake, IA and gives Farhenheit temp.
-            HttpResponseMessage response = client.GetAsync(query).Result;
+            var response = client.GetAsync(query).Result;
             response.EnsureSuccessStatusCode(); //If response is successful move on
-            string s = await response.Content.ReadAsStringAsync(); //Do other things while waiting for response
-            return s;
+            return await response.Content.ReadAsStringAsync();
 
         }
 
@@ -187,10 +186,9 @@ namespace Darpana
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://pro.openweathermap.org/data/2.5/");
             var query = $"forecast?lat=42.6411&lon=-95.2097&APPID={OWAPIKEY}&units=imperial";
-            HttpResponseMessage response = client.GetAsync(query).Result;
+            var response = client.GetAsync(query).Result;
             response.EnsureSuccessStatusCode();
-            string s = await response.Content.ReadAsStringAsync();
-            return s;
+            return await response.Content.ReadAsStringAsync();
         }
 
         //DispatcherTimer method for time module. Updated Time and Date
